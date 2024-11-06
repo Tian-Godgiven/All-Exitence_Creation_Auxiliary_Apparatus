4ddb98dfea9e3cc961d2c4d7d2de84c2f1293ba7
Tian <tiannotsky@gmail.com>
2024-11-06 15:48:16 +0800

    文档编辑

---
+++ All-Exitence_Creation_Auxiliary_Apparatus/components/leftPage/leftPage.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="leftPage" 
+		:style="{
+			width:maxWidth+'px',
+			left:(showWidth - maxWidth < 0? showWidth-maxWidth:0)+'px'
+		}"
+	>
+		123
+	</view>
+</template>
+
+<script setup lang="ts" name="leftPage">
+	let {showWidth,maxWidth} = defineProps(["showWidth","maxWidth"])
+</script>
+
+<style lang="scss">
+	.leftPage{
+		position: absolute;
+		height: 100vh;
+		background-color: black;
+	}
+</style>
---
+++ All-Exitence_Creation_Auxiliary_Apparatus/components/rightPage/rightPage.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="rightPage" 
+		:style="{
+			width:maxWidth+'px', 
+			right:(showWidth - maxWidth < 0? showWidth-maxWidth:0) +'px'
+		}"
+	>
+		999
+	</view>
+</template>
+
+<script setup lang="ts" name="">
+	// 由于右侧页面是从右往左的，因此实际设定的是该页面的right属性
+	let {showWidth,maxWidth} = defineProps(["showWidth","maxWidth"])
+</script>
+
+<style lang="scss">
+	.rightPage{
+		position: absolute;
+		height: 100vh;
+		background-color: red;
+	}
+</style>
