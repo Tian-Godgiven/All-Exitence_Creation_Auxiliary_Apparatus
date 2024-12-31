1e440fd33545dc95617a1f1e59c886b37b845216
Tian <tiannotsky@gmail.com>
2024-12-31 15:51:56 +0800

    进一步改善了关联属性unit值的同步，现在使用watcheffect监听关联属性中的属性值的改变，并修改unit的对应属性名的值，而无需使用reactive减少获取value的步骤

--- All-Exitence_Creation_Auxiliary_Apparatus/src/components/popUps/all-exitence/status/statusValue/relationUnitValue.vue
+++ All-Exitence_Creation_Auxiliary_Apparatus/src/components/popUps/all-exitence/status/statusValue/relationUnitValue.vue
@@ -8,19 +8,24 @@
 </template>
 
 <script setup lang='ts'>
-    import { computed,reactive,ref } from 'vue';
+    import { computed,reactive, watchEffect} from 'vue';
     import statusValueVue from './statusValue.vue';
     const {source,unit} = defineProps(["source","unit"])
     //单元的属性列表
     const unitStatus = computed(()=>{
+        const tmp:any = {}
         for(let key in source){
-            console.log(`unit当中的${key}值是${unit[key]}`)
             if(!unit[key]){
-                unit[key] = reactive({value:source[key].value})
+                unit[key] = source[key].value
             }
+            const status = reactive({value : unit[key]})
+            // 监听 status.value 的变化，并同步更新 unit.name
+            watchEffect(() => {
+                unit[key] = status.value;  // 自动跟踪 status.value 的变化
+            });
+            tmp[key] = status
         }
-        console.log("unit的内容是:",unit)
-        return unit
+        return tmp
     })
 </script>
 
