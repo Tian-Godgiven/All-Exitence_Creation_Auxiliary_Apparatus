609914b3ca891bbe20702c32e6d051743b44595f
Tian <tiannotsky@gmail.com>
2025-04-01 18:58:08 +0800

    修复选择对象为对象时产生的selector的bug

--- src/supportAbility/timeLine/popUp/ChooseExitence/ChooseExitenceOption.vue
+++ src/supportAbility/timeLine/popUp/ChooseExitence/ChooseExitenceOption.vue
@@ -4,58 +4,83 @@
         @change="onChange" 
         @click.stop
         v-if="optionList.length>0" 
-        v-model="item.targetStatus"
+        v-model="item.targetStatus.key"
         :list="optionList"
     />
 </template>
 
 <script setup lang='ts'>
-    import { computed } from 'vue';
+    import { computed, watch } from 'vue';
     import { TItem,EItem } from './chooseExitence';
-import Selector from '@/components/global/Selector.vue';
+    import Selector from '@/components/global/Selector.vue';
 
     const {item,parent} = defineProps<{item:EItem|TItem,parent?:TItem}>()
+    type Option = {
+        label:string,
+        value:string //为该属性的key
+    }
     //选项列表:该对象的时间属性+额外属性中符合条件的部分
-    const optionList = computed<{label:string,value:{time:number,key:string,name:string}}[]>(()=>{
+    const optionList = computed(()=>{
         if(!item.timeStatus)return []
         //分类选择的属性
         const pTargetStatus = parent?.targetStatus
         //该事物的可选属性列表
-        const statusList = item.timeStatus.flatMap((status)=>{
-            //不为其分类当前所选择的时间属性
+        const statusList:Option[] = item.timeStatus.flatMap((status)=>{
+            //不包含其分类当前所选择的时间属性
             if(pTargetStatus && status.__key == pTargetStatus.key){
                 return []
             }
             return {
-                value:{
-                    key:status.__key as string,
-                    time:status.value,
-                    name:status.name as string
-                }, 
+                value:status.__key as string, 
                 label:status.name as string,
             }
         })
         if(statusList.length > 0){
-            //如果分类已经选择了属性，则添加上继承分类属性的选项
+            //如果分类有可选属性，则添加上继承分类属性的选项
             if(pTargetStatus){
                 statusList.unshift({
-                    value:{
-                        key:"inherit",
-                        time:null,
-                        name:""
-                    },
+                    value:"inherit",
                     label:"继承分类"
                 })
             }
-            //设置为默认选项(第一个选项)
-            item.targetStatus = statusList[0].value
         }
         return statusList
     })
-    //改变选择时，还会改变其事物选择的属性
+    //默认选择第一个选项
+    watch(optionList,()=>{
+        if(optionList.value.length > 0){
+            //设置为默认选项(第一个选项)
+            item.targetStatus.key = optionList.value[0].value
+        }
+    },{
+        immediate:true
+    })
+    
     function onChange(){
+        //填充其余的属性
+        const key = item.targetStatus.key
+        //继承
+        if(key == "inherit"){
+            item.targetStatus = {
+                key,
+                name:"继承属性",
+                time:0
+            }
+        }
+        //找到这个属性的其他数据
+        else{
+            const status = item.timeStatus.find(status=>status.__key == key)
+            if(status){
+                item.targetStatus = {
+                    key,
+                    name:status.name as string,
+                    time:status.value
+                }
+            }
+        }
+        
+        //改变选择时，还会改变其事物选择的属性
         if('child' in item){
-            console.log("改变子元素了",item)
             const targetStatus = item.targetStatus;
             item.child.forEach((child)=>{
                 //分组的情况
