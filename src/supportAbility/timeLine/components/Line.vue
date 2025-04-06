9ed63ecd0b44df89aada44217632fa9edcf17401
Tian <tiannotsky@gmail.com>
2025-04-06 15:59:17 +0800

    逻辑转移到linetick上

--- src/supportAbility/timeLine/components/Line.vue
+++ src/supportAbility/timeLine/components/Line.vue
@@ -2,8 +2,8 @@
 <svg class="line">
     <path :d="path" stroke="black" stroke-width="2" fill="none"/>
     <g stroke="black">
-        <LineTick v-for="tick in tickList" 
-            :tick></LineTick>
+        <LineTick :index = 0></LineTick>
+        <LineTick v-for="index in tickNum" :index></LineTick>
     </g>
 </svg>
 </template>
@@ -11,15 +11,8 @@
 <script setup lang='ts'>
     import { computed, inject } from 'vue';
     import LineTick from './LineTick.vue';
-import { TimeRule } from '@/supportAbility/customTime/customTime';
-import { getStartTickInfo, getTickInfo } from '../timeLine';
 
     const {lineWidth} = defineProps<{lineWidth:number}>()
-
-    const timeRule = inject<TimeRule>("timeRule","date")
-    const minUnit = inject<string>("minUnit")
-    //开始时间
-    const startTime = inject<number>("startTime",0)
     const setting = inject("timeLineSetting",{
         space:10,
         equelUnit:1
@@ -31,39 +24,10 @@ import { getStartTickInfo, getTickInfo } from '../timeLine';
     })
 
     //总刻度列表
-    const tickList = computed(()=>{ 
+    const tickNum = computed(()=>{ 
         //刻度总数量
-        const tickNum = lineWidth/(setting.space/setting.equelUnit)
-        console.log(tickNum)
-        const tickList = []
-        for(let i = 0;i<tickNum;i++){
-            //刻度参数
-            const x = (i*setting.space)/setting.equelUnit
-            const value = startTime + i
-            //刻度info
-            let info
-            if(x == 0){
-                info = getStartTickInfo(value,timeRule,minUnit)
-            }
-            else{
-                info = getTickInfo(value,timeRule,minUnit)
-            }
-            //如果这个值能够获得一个Info，则默认渲染它
-            if(info.text){
-                tickList.push({
-                    x,
-                    ...info
-                })
-            }
-            //否则仅渲染倍数的tick
-            else if(value%setting.equelUnit == 0){
-                tickList.push({
-                    x,
-                    ...info
-                })
-            }
-        }
-        return tickList
+        const tickNum = Math.floor(lineWidth/(setting.space/setting.equelUnit))
+        return tickNum
     })
 </script>
 
--- src/supportAbility/timeLine/components/LineTick.vue
+++ src/supportAbility/timeLine/components/LineTick.vue
@@ -1,23 +1,64 @@
 <template>
-    <line :stroke-width="tick.width" 
-        :x1="tick.x" :y1="tick.height" :x2="tick.x" :y2="-tick.height" />
-    <text v-if="tick.text" 
-        class="text"
-        v-for="item,index in tick.text"
-        :x="tick.x-5" :y="20*(index+1)">
-        {{ item }}
-    </text>
+    <g v-if="ifShow">
+        <line :stroke-width="info.width" 
+        :x1="tick.x" :y1="info.height" :x2="tick.x" :y2="-info.height" />
+        <text v-if="info.text" 
+            class="text"
+            v-for="item,index in info.text"
+            :x="tick.x-5" :y="20*(index+1)">
+            {{ item }}
+        </text>
+    </g>
+    
 </template>
 
 <script setup lang='ts'>
-    const {tick} = defineProps<{
-        tick:{
-            x:number,
-            width:number,
-            height:number,
-            text:string[]
-        }
+import { TimeRule } from '@/supportAbility/customTime/customTime';
+import { computed, inject } from 'vue';
+import { getStartTickInfo, getTickInfo } from '../timeLine';
+
+    const {index} = defineProps<{
+        index:number
     }>()
+
+    const timeRule = inject<TimeRule>("timeRule","date")
+    const minUnit = inject<string>("minUnit")
+    //开始时间
+    const startTime = inject<number>("startTime",0)
+    const setting = inject("timeLineSetting",{
+        space:10,
+        equelUnit:1
+    })
+
+    const tick = computed(()=>{
+         //刻度参数
+        const x = (index*setting.space)/setting.equelUnit
+        const value = startTime + index
+        return {
+            x,value
+        }
+    })
+    const info = computed(()=>{
+        //刻度info
+        let info
+        if(tick.value.x == 0){
+            info = getStartTickInfo(tick.value.value,timeRule,minUnit)
+        }
+        else{
+            info = getTickInfo(tick.value.value,timeRule,minUnit)
+        }
+        return info
+    })
+    const ifShow = computed(()=>{
+        //如果这个值能够获得一个Info,或者是倍数的tick，则默认渲染它
+        if(info.value.text || tick.value.value%setting.equelUnit == 0){
+            return true
+        }
+        return false
+    })
+
+   
+            
 </script>   
 
 <style scoped lang='scss'>
