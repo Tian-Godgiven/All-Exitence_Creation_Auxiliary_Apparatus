ed9ef2a2f579d30e36e78f664c11cb4e53cfd63c
Tian <tiannotsky@gmail.com>
2025-04-27 18:22:55 +0800

    为菜单组件添加了clickoutside功能

--- src/components/global/Menu.vue
+++ src/components/global/Menu.vue
@@ -1,16 +1,15 @@
 <template>
 <div class="menu">
     <slot :switchMenu></slot>
-    <div v-show="showMenu" class="menuContent" :style="style">
+    <div v-show="showMenu" ref="menuContent" class="menuContent" :style="style">
         <slot name="menu"></slot>
     </div>
 </div>
 </template>
 
 <script setup lang='ts'>
-    import { computed, ref } from 'vue';
-    const {title,start=false,vertical="left",horizen="bottom"} = defineProps<{
-        title?:string,
+    import { computed, onMounted, onUnmounted, ref, useTemplateRef } from 'vue';
+    const {start=false,vertical="left",horizen="bottom"} = defineProps<{
         start?:boolean,
         vertical:"left"|"center"|"right",
         horizen:"top"|"bottom"
@@ -44,14 +43,25 @@
             showMenu.value = !showMenu.value
         }
     }
+    //点击到外部关闭
+    const content = useTemplateRef("menuContent")
+    const clickoutside = function(e:MouseEvent){
+        // 判断点击的目标是否是菜单内容区域的外部
+        if (content.value && !content.value.contains(e.target as Node)) {
+            switchMenu(false)//隐藏
+        }
+    }
+    onMounted(()=>{
+        document.addEventListener("click",clickoutside)
+    })
+    onUnmounted(()=>{
+        document.removeEventListener("click",clickoutside)
+    })
 </script>
 
 <style scoped lang='scss'>
 .menu{
     position: relative;
-    .title{
-
-    }
     .menuContent{
         position: absolute;
     }
